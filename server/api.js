473f410ac709c14d418149e1eb10ac3a3f2713f4
Ikram Chehouani <60435296+ikramchehouani@users.noreply.github.com>
2023-09-05 15:43:12 +0200

    need to access json instead of html

--- server/api.js
+++ server/api.js
@@ -1,27 +1,41 @@
 const express = require('express');
 const router = express.Router();
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb'); // Import ObjectId
 
 // Define your MongoDB connection string
-const uri = "mongodb://icram:nonmaislol@clusterFirst";
+const uri = "mongodb://icram:nonmaislol@clusterFirst"; // Update with your correct MongoDB URI
 
+// Define the route to retrieve a message by a hardcoded _id
 router.get('/get-message', async (req, res) => {
   try {
+    // Hardcoded _id (replace this with your actual _id)
+    const hardcodedId = "64f5d3ebfed127df07f4c996";
+
+    // Create a new MongoClient instance
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    // Connect to MongoDB
     await client.connect();
 
+    // Access the "ika" database and "retrievers" collection
     const database = client.db("ika");
     const collection = database.collection("retrievers");
 
-    // Query your MongoDB collection to get the message
-    const result = await collection.findOne({ key: "message" });
+    // Query your MongoDB collection to get the message by _id
+    const result = await collection.findOne({ _id: new ObjectId(hardcodedId) });
 
-    // Send the message as a JSON response
-    res.json({ message: result.message });
+    if (result) {
+      // Send the message as a JSON response if found
+      res.json({ message: result.message });
+    } else {
+      // If no result is found, send an appropriate response
+      res.status(404).json({ error: "Message not found" });
+    }
   } catch (error) {
     console.error("Error:", error);
     res.status(500).json({ error: "An error occurred" });
   } finally {
+    // Ensure that the client connection is closed
     client.close();
   }
 });
